75cf416cb494c72128ebab159bc0d3b4ee411541
Hassan chaudhry <hassan.chaudhry@westjet.com>
2020-03-07 18:11:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let userList = [];
+let messageList = [];
+let nickCheck = '/nick';
+let colorCheck = '/nickcolor';
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+
+io.on('connection', (socket) => {
+    console.log("a user connected");
+
+    let user = { name: '', color: ''};
+    socket.on('chat message', (msg) => {
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let currentTime = hours + ":" + minutes + ":" + seconds;
+        let checkText = msg.split(/\s+/);
+        let checkAlphaNum = /^[a-z0-9]+$/i;
+        let checkHex = /^[A-F0-9]{6}/i;
+        let message = {
+            time: currentTime,
+            user: user.name,
+            color: user.color,
+            msg: msg
+        };
+        console.log(checkText);
+
+        if (checkText[0] === nickCheck){
+            let newNickname = msg.substring(6);
+
+            if(checkAlphaNum.test(newNickname)){
+                 if(userList.find((name) => {
+                     return name.name === newNickname
+                 }) === undefined && newNickname.length > 0){
+                     user.name = newNickname;
+                     socket.emit('nameChanged', user);
+                     io.emit('updateUserList', userList);
+                 } else {
+                     
+                 }
+            }
+        } else if (checkText[0] === colorCheck){
+            let newNickColor = msg.substring(11);
+
+            if(checkHex.test(newNickColor)){
+                user.color = '#' + newNickColor;
+                socket.emit('colorChanged', user);
+                io.emit('updateUserList', userList);
+            }
+        } else {
+            if (messageList.length <= 200){
+                messageList.push(message);
+            } else if (messageList.length > 200){
+                messageList.shift();
+                messageList.push(message);
+            }
+            io.emit('chat message', message);
+        }
+    });
+
+    socket.on('cookies', (msg) => {
+        let cookieName = msg;
+        let userNum = Math.floor(Math.random() * 11)
+        let newUser = {name: 'User ', color: '##999999'};
+        console.log(cookieName)
+
+        if (cookieName !== ''){
+            if(userList.find((cookie) => {
+                return cookie.name === cookieName
+            }) === undefined){
+                user.name = cookieName;
+                userList.push(user);
+            } else {
+                user = newUser;
+                user.name = user.name + userNum;
+                userList.push(user);
+            }
+        } else {
+            user = newUser;
+            user.name = user.name + userNum;
+            userList.push(user);
+        }
+        
+        for(index of messageList){
+            socket.emit('chat message', index);
+        }
+        socket.emit('nameChanged', user);
+        io.emit('updateUserList', userList);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+        userList.splice(userList.findIndex((quitter) => {
+            return quitter.name === user.name;
+        }), 1);
+        io.emit('updateUserList', userList);
+    });
+});
---
+++ public/client.js
@@ -0,0 +1,43 @@
+$(() => {
+    let socket = io();
+    let cookie = Cookies.get('name');
+    socket.emit('cookies', cookie);
+    let currentUser;
+
+    $('form').submit(function(){
+       // e.preventDefault();
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    socket.on('updateUserList', (msg) =>{
+        $('#users').empty();
+        for (let user of msg){
+            $('#users').append($('<li>').text(user.name));
+        }
+    });
+
+    socket.on('chat message', (msg) => {
+        let displayMessage = msg.time + ' <span>' + msg.user + '</span>: ' + msg.msg
+        if(msg.user === currentUser){
+            displayMessage = '<b>' + displayMessage + '</b>';
+        }
+        $('#messages').append($('<li>').html(displayMessage));
+        $('#messages').find('li:last').find('span').css('color', msg.color);
+
+    });
+
+    socket.on('nameChanged', (msg) =>{
+        let displayMessage = "You are now <span>" + msg.name + "</span>.";
+        $('#messages').append($('<li>').html(displayMessage));
+        currentUser = msg.name;
+        Cookies.set('name', msg.name, { expires: 365});
+    });
+
+    socket.on('colorChanged', (msg) =>{
+        $('#messages').find('li:last').find('span').css('color',msg.color);
+        currentUser = msg.name;
+        Cookies.set('name', msg.name, { expires: 365});
+    });
+});
